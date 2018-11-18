46fa75e62bc7a82f67053a0c13738e48bc673294
MADHAN <madhansmart7404@gmail.com>
2018-11-18 15:59:25 +0530

    second

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
+import Footer from './Components/Footer/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <Header />
+            <Banner />
+        </div>
+    );
+  }
+}
+
+export default App;
